8c0a49cd133ff4d9c419cc85feb981ec984ac3f6
otavio-araujo <otavio_araujo@hotmail.com>
2024-07-19 18:58:54 -0300

    Installs Debugbar, Creates pagination.php component, alters pagination.php, and improves query performance in PostResource.php.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -15,7 +15,7 @@ class PostController extends Controller
     public function index()
     {
         return inertia('Posts/Index', [
-            'posts' => PostResource::collection(Post::all()),
+            'posts' => PostResource::collection(Post::paginate()),
         ]);
     }
 
--- resources/js/Pages/Posts/Index.vue
+++ resources/js/Pages/Posts/Index.vue
@@ -2,10 +2,11 @@
     <app-layout>
         <Container>
             <ul class="divide-y">
-                <li v-for="post in posts" :key="post.id" class="px-2 py-4">
+                <li v-for="post in posts.data" :key="post.id" class="px-2 py-4">
                     <span class="font-bold text-lg">{{ post.title }}</span>
                 </li>
             </ul>
+            <Pagination :meta="posts.meta"/>
         </Container>
     </app-layout>
 </template>
@@ -13,6 +14,7 @@
 <script lang="ts" setup>
 import AppLayout from "@/Layouts/AppLayout.vue";
 import Container from "@/Components/Container.vue";
+import Pagination from "@/Components/Pagination.vue";
 
 defineProps(['posts']);
 </script>

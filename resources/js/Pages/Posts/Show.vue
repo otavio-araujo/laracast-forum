5a95080d2d4869e45461b6771d87a3f75ec63221
otavio-araujo <otavio_araujo@hotmail.com>
2024-07-21 22:17:31 -0300

    Add Comments frontend done.

--- resources/js/Pages/Posts/Show.vue
+++ resources/js/Pages/Posts/Show.vue
@@ -12,6 +12,17 @@
             <div>
                 <h2 class="mt-12 text-xl font-semibold">Comments</h2>
 
+                <form v-if="$page.props.auth.user" class="mt-4" @submit.prevent="addComment">
+                    <div>
+                        <InputLabel class="sr-only" for="body">Comment</InputLabel>
+                        <TextArea id="body" v-model="commentForm.body" placeholder="Speak your mind spock..." rows="4"/>
+                        <InputError :message="commentForm.errors.body" class="mt-1"/>
+                    </div>
+
+                    <PrimaryButton :disabled="commentForm.processing" class="mt-3" type="submit">Add Comment
+                    </PrimaryButton>
+                </form>
+
                 <ul class="divide-y">
                     <li v-for="comment in comments.data" :key="comment.id" class="px-2 py-4 mt-4">
                         <Comment :comment="comment"/>
@@ -31,9 +42,23 @@ import {computed} from "vue";
 import Pagination from "@/Components/Pagination.vue";
 import {relativeDate} from "@/Utilities/date.js";
 import Comment from "@/Components/Comment.vue";
+import InputLabel from "@/Components/InputLabel.vue";
+import PrimaryButton from "@/Components/PrimaryButton.vue";
+import {useForm} from "@inertiajs/vue3";
+import TextArea from "@/Components/TextArea.vue";
+import InputError from "@/Components/InputError.vue";
 
 const props = defineProps(['post', 'comments']);
 
 const formattedDate = computed(() => relativeDate(props.post.created_at));
+
+const commentForm = useForm({
+    body: '',
+});
+
+const addComment = () => commentForm.post(route('posts.comments.store', props.post.id), {
+    preserveScroll: true,
+    onSuccess: () => commentForm.reset(),
+});
 
 </script>

e201ac2ea39d90042be38f3db7e0856b22d570a7
otavio-araujo <otavio_araujo@hotmail.com>
2024-07-22 23:02:13 -0300

    FEATURE:POSTS - Frontend.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -43,7 +43,7 @@ class PostController extends Controller
      */
     public function create()
     {
-        //
+        return inertia('Posts/Create');
     }
 
     /**
---
+++ resources/js/Pages/Posts/Create.vue
@@ -0,0 +1,45 @@
+<template>
+    <AppLayout title="Create a Post">
+        <Container>
+            <h1 class="text-2xl font-bold">Create a Post</h1>
+
+            <form @submit.prevent="createPost">
+
+                <div class="mt-6">
+                    <InputLabel class="sr-only" for="title">Title</InputLabel>
+                    <TextInput id="title" v-model="form.title" class="w-full" placeholder="Give it a great title..."/>
+                    <InputError :message="form.errors.title" class="mt-1"/>
+                </div>
+                <div class="mt-3">
+                    <InputLabel class="sr-only" for="body">Body</InputLabel>
+                    <TextArea id="body" v-model="form.body" rows="25"/>
+                    <InputError :message="form.errors.body" class="mt-1"/>
+                </div>
+                <div class="mt-3">
+                    <PrimaryButton type="submit">Create Post</PrimaryButton>
+                </div>
+
+            </form>
+        </Container>
+    </AppLayout>
+</template>
+
+<script setup>
+
+import {useForm} from "@inertiajs/vue3";
+import PrimaryButton from "@/Components/PrimaryButton.vue";
+import InputLabel from "@/Components/InputLabel.vue";
+import AppLayout from "@/Layouts/AppLayout.vue";
+import TextInput from "@/Components/TextInput.vue";
+import Container from "@/Components/Container.vue";
+import InputError from "@/Components/InputError.vue";
+import TextArea from "@/Components/TextArea.vue";
+
+const form = useForm({
+    title: '',
+    body: '',
+});
+
+const createPost = () => form.post(route('posts.store'));
+
+</script>

c26d16621e1e85aa11a012fb9a13b5981a3d4816
otavio-araujo <otavio_araujo@hotmail.com>
2024-07-22 21:02:47 -0300

    Implement the confirmation modal.

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -7,6 +7,7 @@ import Dropdown from '@/Components/Dropdown.vue';
 import DropdownLink from '@/Components/DropdownLink.vue';
 import NavLink from '@/Components/NavLink.vue';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
+import ConfirmationModalWrapper from "@/Components/ConfirmationModalWrapper.vue";
 
 defineProps({
     title: String,
@@ -278,5 +279,8 @@ const menu = [
                 <slot/>
             </main>
         </div>
+
+        <ConfirmationModalWrapper/>
+
     </div>
 </template>
